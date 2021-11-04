cdcda3d7414a7fe2a9ff50ed945db448fd9613fa
Henrique Lima <henriquelimaribeiro4@gmail.com>
2021-11-04 19:56:12 -0300

    Update project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate : validateUuid } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,56 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
 
+  if(!user) {
+    return response.status(404).json({error: "Not found user"});
+  }
+  request.user = user;
+  return next();
+}
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length == 10) {
+  return response.status(403).json({error: "Message error"});
+  }
+
+  else if(user.pro === true) {
+  return next()
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((user) => user.user === id);
+
+  if(todo && validateUuid(id)) {
+    request.user = user;
+    request.todo = todo;
+    return next();
+  } else {
+    return response.status(404).json({error: "error id"});
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const VerifyIfExistUser = users.find((user) => user.id === id);
+
+  if(!VerifyIfExistUser) {
+    return response.status(404).json({error: "User not found"});
+  }
+  request.user = VerifyIfExistUser;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -75,6 +112,7 @@ app.post('/todos', checksExistsUserAccount, checksCreateTodosUserAvailability, (
   const { title, deadline } = request.body;
   const { user } = request;
 
+  // create a new todo
   const newTodo = {
     id: uuidv4(),
     title,
---
+++ test.js
@@ -0,0 +1,10 @@
+// let listaDeCasamento = ["Amor", "Casa", "Carro", "Carinho",];
+
+// console.log(listaDeCasamento.length());
+
+// listaDeCasamento.length()
+
+const fruits = ["Amor", "Casa", "Carro", "Carinho"]
+
+
+console.log(fruits.length);
